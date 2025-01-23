c16a5324367380f837fe53e06de22ed2dfab97ad
kemalmusthafa <72489722+kemalmusthafa@users.noreply.github.com>
2025-01-23 21:15:59 +0700

    Update Dropdown Bug (#38)
    * Add UI Dashboard and CTA
    
    * Update dropdown pada sidebar
    
    * Add auth verify login and regist for user
    
    * Updated uregister and ulogin from api
    
    * Resolve Conflict
    
    * Add GetSession FE
    
    * Updated Resolve Bug PLogin
    
    * Updated Resolve Bug ULogin
    
    * Update CTA
    
    * Added Footer, ForgotPassword, Profile, and updated dropdown mobile
    
    * Updated navbar, footer and added contact and price FE
    
    * update sidebar and footer
    
    * Add profile, resetPasswordUser, dashboard
    
    * Add authGuard and update navbar footer except, fetching data dashboard
    
    * resolve conflict FE
    
    * Updated dashboard
    
    * Updated profile
    
    * Add eventactive, eventfinish, ticketsales, and update loading, authGuard, link back to dashboard
    
    * Updated card eventactive, eventfinish, profileUser, profilePromotor
    
    * Add promotorGuard and userGuard for protect page, update transaction log
    
    * Update bug minor darkmode setting, dropdown hp, loading
    
    * Update bug dropdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Meta tag viewport ditambahkan langsung di sini */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body
